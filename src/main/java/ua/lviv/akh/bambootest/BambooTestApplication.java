c26849572e66929d927e3511a173977147b5c50d
andrii.khoma@externals.adidas.com <andrewkhoma@gmail.com>
2020-06-03 22:46:01 +0300

    init

---
+++ src/main/java/ua/lviv/akh/bambootest/BambooTestApplication.java
@@ -0,0 +1,13 @@
+package ua.lviv.akh.bambootest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambooTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BambooTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/lviv/akh/bambootest/BambooTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.akh.bambootest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BambooTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
